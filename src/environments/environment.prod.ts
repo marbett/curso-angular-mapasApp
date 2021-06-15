d84f169fbef12c8ff89c903b6129190c83b7f0d5
Marbett Hedderich <marbett@gmail.com>
2021-06-15 15:13:32 +0200

    mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken:'pk.eyJ1IjoibWFyYmV0dCIsImEiOiJja3B4dWQya2gwMTlyMnVqc2k1dDd2ZGFqIn0.314n1FiQA5-7yBZu1W7g7w'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken:'pk.eyJ1IjoibWFyYmV0dCIsImEiOiJja3B4dWQya2gwMTlyMnVqc2k1dDd2ZGFqIn0.314n1FiQA5-7yBZu1W7g7w'
 };
 
 /*
